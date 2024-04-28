ff8d80e6d8a71797b247f44bbc4d71a4be5e9fa0
JingDanGao <166778780+JingDanGao@users.noreply.github.com>
2024-04-29 00:26:57 +0800

    Add files via upload

---
+++ scenery.js
@@ -0,0 +1,14 @@
+// 初始化 swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
